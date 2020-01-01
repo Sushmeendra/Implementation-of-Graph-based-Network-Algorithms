99909ad505c4827bd640c1d218a7635c8993e466
Sushmeendra <sushmeendra123@gmail.com>
2020-01-01 15:50:17 +0530

    Add files via upload

---
+++ SIS_Model.py
@@ -0,0 +1,38 @@
+
+
+import matplotlib.pylab as plt
+
+N = 1000
+S = N - 1
+I = 1
+beta = 1
+gamma = 0.0
+
+sus = []
+inf = []
+
+def infection(S, I, N):
+    for t in range (0, 1000):
+        S = S - (beta*S*I/N) + gamma * I
+        I = I + (beta*S*I/N) - gamma * I
+
+        sus.append(S)
+        inf.append(I)
+
+
+infection(S, I, N)
+
+figure = plt.figure()
+figure.canvas.set_window_title('SIS model')
+
+inf_line, =plt.plot(inf, label='I(t)')
+
+sus_line, = plt.plot(sus, label='S(t)')
+plt.legend(handles=[inf_line, sus_line])
+
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+plt.xlabel('T')
+plt.ylabel('N')
+
+plt.show()
